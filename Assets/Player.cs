9e69df97a1fba056d07e2df2f26bf32f06552cf8
I_Jemin <i_jemin@hotmail.com>
2017-01-17 17:31:32 +0900

    Init

---
+++ Assets/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ToggleEvent : UnityEvent<bool>{}
+public class Player : NetworkBehaviour {
+	
+	[SerializeField] ToggleEvent onToggleShared;
+	[SerializeField] ToggleEvent onToggleLocal;
+	[SerializeField] ToggleEvent onToggleRemote;
+
+	GameObject mainCamera;
+	void Start()
+	{
+		mainCamera = Camera.main.gameObject;
+		EnablePlayer();
+	}
+	void DisablePlayer()
+	{
+		if(isLocalPlayer)
+			mainCamera.SetActive(true);
+
+		onToggleShared.Invoke(false);
+		
+		if(isLocalPlayer)
+			onToggleLocal.Invoke(false);
+		else
+			onToggleRemote.Invoke(false);
+	}
+	void EnablePlayer()
+	{
+		if(isLocalPlayer)
+			mainCamera.SetActive(false);
+	
+		onToggleShared.Invoke(true);
+		
+		if(isLocalPlayer)
+			onToggleLocal.Invoke(true);
+		else
+			onToggleRemote.Invoke(true);
+	}
+
+}
